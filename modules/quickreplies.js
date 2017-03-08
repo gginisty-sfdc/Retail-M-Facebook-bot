82237a38eb601a8d1fc1615c133a9d03ffa40eeb
shocks13 <shocks13@gmail.com>
2017-03-08 16:45:30 +0000

    first commit

--- modules/quickreplies.js
+++ modules/quickreplies.js
@@ -6,13 +6,16 @@ let salesforce = require('./salesforce'),
 
 exports.camprelax = (sender) => {
 	console.log('camprelax');
+    messenger.send({text: `camprelax`}, sender);
 };
 
 exports.climbmountain = (sender) => {
     console.log('climbmountain');
+    messenger.send({text: `climbmountain`}, sender);
 };
 
 exports.perfectclimbing = (sender) => {
     console.log('perfectclimbing');
+    messenger.send({text: `perfectclimbing`}, sender);
 };
 
